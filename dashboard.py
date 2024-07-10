6d97a85634d34538ac18b2d1b3e799eb6c83babc
ConstAbz <kabzalimov@mail.ru>
2024-07-10 20:15:57 +0300

    new file:   .gitignore 	modified:   dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,196 +1,144 @@
 import datetime
-
 import streamlit as st
 import pandas as pd
 import psycopg2
 
+# Выбор схемы базы данных
 schema = st.selectbox('Select schema', ('small', 'prod'))
 
-conn = psycopg2.connect('user=app password=verysecretpassword host=localhost dbname=appdb', options=f'-c search_path={schema}',)
+# Установка соединения с базой данных с использованием выбранной схемы
+conn = psycopg2.connect('user=app password=verysecretpassword host=localhost dbname=appdb', options=f'-c search_path={schema}')
 
-locations = {}
-store_locations = {}
-warehouses = {}
-warehouse_locations = {}
-warehouse_shelves = {}
-products_on_shelves = {}
-product_check_positions = []
-product_categories = {}
-categories = {}
-products = {}
-checks = {}
-checks_stores = {}
-shelves = {}
-stores = {}
-supplies = {}
-supplies_warehouses = []
-supplies_stores = {}
-external_supplies = {}
-external_supplies_warehouses = []
-supplies_products = []
-external_supplies_products = []
+def execute_query(query, params=None):
+    with conn.cursor() as cur:
+        cur.execute(query, params)
+        return cur.fetchall()
 
-with conn as txn:
-    cur = conn.cursor()
-    cur.execute('select * from locations')
-    for row in cur.fetchall():
-        locations[row[0]] = row[1]
-    cur.execute('select * from store_locations')
-    for row in cur.fetchall():
-        store_locations[row[0]] = row[1]
-    cur.execute('select * from warehouses')
-    for row in cur.fetchall():
-        warehouses[row[0]] = {'name': row[1]}
-    cur.execute('select * from warehouse_locations')
-    for row in cur.fetchall():
-        warehouse_locations[row[0]] = row[1]
-    cur.execute('select * from warehouse_shelves')
-    for row in cur.fetchall():
-        warehouse_shelves[row[0]] = row[1]
-    cur.execute('select * from products_on_shelves')
-    for row in cur.fetchall():
-        products_on_shelves[row[2]] = {'count': row[1], 'product_id': row[0]}
-    cur.execute('select * from product_check_positions')
-    for row in cur.fetchall():
-        product_check_positions.append((row[0], row[1], row[2]))
-    cur.execute('select * from product_categories')
-    for row in cur.fetchall():
-        product_categories[row[0]] = row[1]
-    cur.execute('select * from categories')
-    for row in cur.fetchall():
-        categories[row[0]] = row[1]
-    cur.execute('select * from products')
-    for row in cur.fetchall():
-        products[row[0]] = {'name': row[1]}
-    cur.execute('select * from checks')
-    for row in cur.fetchall():
-        checks[row[0]] = {'issue_date': row[1]}
-    cur.execute('select * from checks_stores')
-    for row in cur.fetchall():
-        checks_stores[row[0]] = row[1]
-    cur.execute('select * from shelves')
-    for row in cur.fetchall():
-        shelves[row[0]] = row[1]
-    cur.execute('select * from stores')
-    for row in cur.fetchall():
-        stores[row[0]] = {'name': row[1]}
-    cur.execute('select * from supplies')
-    for row in cur.fetchall():
-        supplies[row[0]] = {'finish_date': row[1]}
-    cur.execute('select * from supplies_stores')
-    for row in cur.fetchall():
-        supplies_stores[row[0]] = row[1]
-    cur.execute('select * from external_supplies')
-    for row in cur.fetchall():
-        external_supplies[row[0]] = {'finish_date': row[1]}
-    cur.execute('select * from supplies_warehouses')
-    for row in cur.fetchall():
-        supplies_warehouses.append((row[0], row[1], row[2]))
-    cur.execute('select * from external_supplies_warehouses')
-    for row in cur.fetchall():
-        external_supplies_warehouses.append((row[0], row[1], row[2]))
-    cur.execute('select * from external_supplies_products')
-    for row in cur.fetchall():
-        external_supplies_products.append((row[0], row[1], row[2]))
-    cur.execute('select * from supplies_products')
-    for row in cur.fetchall():
-        supplies_products.append((row[0], row[1], row[2]))
+def get_data_dict(query):
+    data = execute_query(query)
+    return {row[0]: row[1:] for row in data}
 
-for store, opts in stores.items():
-    for location_id, store_id in store_locations.items():
-        if store_id == store:
-            opts['location'] = locations[location_id]
+# Запросы для получения данных
+categories = get_data_dict('SELECT id, category_name FROM categories')
 
-for warehouse, opts in warehouses.items():
-    for location_id, warehouse_id in warehouse_locations.items():
-        if warehouse_id == warehouse:
-            opts['location'] = locations[location_id]
+def get_product_info():
+    query = '''
+    SELECT p.id, p.name, pc.category_id, c.category_name, pos.shelve_id, pos.count
+    FROM products p
+    JOIN product_categories pc ON p.id = pc.product_id
+    JOIN categories c ON pc.category_id = c.id
+    LEFT JOIN products_on_shelves pos ON p.id = pos.product_id
+    '''
+    data = execute_query(query)
+    products = {}
+    for row in data:
+        product_id, name, category_id, category_name, shelve_id, count = row
+        if product_id not in products:
+            products[product_id] = {
+                'name': name,
+                'category': category_name,
+                'shelves': []
+            }
+        if shelve_id is not None:
+            products[product_id]['shelves'].append({'shelve_id': shelve_id, 'count': count})
+    return products
 
-for warehouse, opts in warehouses.items():
-    for shelve_id, warehouse_id in warehouse_shelves.items():
-        if warehouse_id == warehouse:
-            if 'shelves' not in opts:
-                opts['shelves'] = []
-            opts['shelves'].append(shelve_id)
+def get_checks():
+    query = '''
+    SELECT c.id, c.issue_date, cp.product_id, cp.product_count, cs.store_id
+    FROM checks c
+    JOIN checks_stores cs ON c.id = cs.check_id
+    JOIN product_check_positions cp ON c.id = cp.check_id
+    '''
+    data = execute_query(query)
+    checks = {}
+    for row in data:
+        check_id, issue_date, product_id, product_count, store_id = row
+        if check_id not in checks:
+            checks[check_id] = {
+                'issue_date': issue_date,
+                'store_id': store_id,
+                'positions': []
+            }
+        checks[check_id]['positions'].append({
+            'product_id': product_id,
+            'product_count': product_count
+        })
+    return checks
 
-for product, opts in products.items():
-    for shelve, data in products_on_shelves.items():
-        if product == data['product_id']:
-            if 'shelves' not in opts:
-                opts['shelves'] = []
-            opts['shelves'].append({'shelve_id': shelve, 'count': data['count']})
+def get_supplies(query):
+    data = execute_query(query)
+    supplies = {}
+    for row in data:
+        supply_id, finish_date, store_id, warehouse_id, shelve_id, product_id, product_count = row
+        if supply_id not in supplies:
+            supplies[supply_id] = {
+                'finish_date': finish_date,
+                'store_id': store_id,
+                'shelves': [],
+                'positions': []
+            }
+        if warehouse_id is not None and shelve_id is not None:
+            supplies[supply_id]['shelves'].append({
+                'warehouse_id': warehouse_id,
+                'shelve_id': shelve_id
+            })
+        if product_id is not None and product_count is not None:
+            supplies[supply_id]['positions'].append({
+                'product_id': product_id,
+                'product_count': product_count
+            })
+    return supplies
 
-for product, opts in products.items():
-    category_id = product_categories[product]
-    category_name = categories[category_id]
-    opts['category'] = category_name
+products = get_product_info()
+checks = get_checks()
+supplies = get_supplies('''
+    SELECT s.id, s.finish_date, ss.store_id, sw.warehouse_id, sw.shelve_id, sp.product_id, sp.product_count
+    FROM supplies s
+    LEFT JOIN supplies_stores ss ON s.id = ss.supply_id
+    LEFT JOIN supplies_warehouses sw ON s.id = sw.supply_id
+    LEFT JOIN supplies_products sp ON s.id = sp.supply_id
+''')
+external_supplies = get_supplies('''
+    SELECT es.id, es.finish_date, NULL, esw.warehouse_id, esw.shelve_id, esp.product_id, esp.product_count
+    FROM external_supplies es
+    LEFT JOIN external_supplies_warehouses esw ON es.id = esw.supply_id
+    LEFT JOIN external_supplies_products esp ON es.id = esp.supply_id
+''')
 
-for check, opts in checks.items():
-    opts['store_id'] = checks_stores[check]
-    opts['positions'] = []
-    for position in product_check_positions:
-        if position[0] == check:
-            opts['positions'].append({'product_count': position[1], 'product_id': position[2]})
-
-for supply, opts in supplies.items():
-    opts['store_id'] = supplies_stores[supply]
-    opts['shelves'] = []
-    opts['positions'] = []
-    for shelve in supplies_warehouses:
-        if shelve[0] == supply:
-            opts['shelves'].append({'warehouse_id': shelve[1], 'shelve_id': shelve[2]})
-    for position in supplies_products:
-        if position[0] == supply:
-            opts['positions'].append({'product_id': position[1], 'product_count': position[2]})
-
-for supply, opts in external_supplies.items():
-    opts['shelves'] = []
-    opts['positions'] = []
-    for shelve in external_supplies_warehouses:
-        if shelve[0] == supply:
-            opts['shelves'].append({'warehouse_id': shelve[1], 'shelve_id': shelve[2]})
-    for position in external_supplies_products:
-        if position[0] == supply:
-            opts['positions'].append({'product_id': position[1], 'product_count': position[2]})
-
+# Создание событий для графика
 events = {}
+for check_id, opts in checks.items():
+    events.setdefault(opts['issue_date'], []).append({'type': 'check', 'data': {'check_id': check_id, **opts}})
+for supply_id, opts in supplies.items():
+    events.setdefault(opts['finish_date'], []).append({'type': 'supply', 'data': {'supply_id': supply_id, **opts}})
+for supply_id, opts in external_supplies.items():
+    events.setdefault(opts['finish_date'], []).append({'type': 'external_supply', 'data': {'supply_id': supply_id, **opts}})
 
-for check, opts in checks.items():
-    if opts['issue_date'] not in events:
-        events[opts['issue_date']] = []
-    events[opts['issue_date']].append({'type': 'check', 'data': {'check_id': check, **opts}})
-
-for supply, opts in supplies.items():
-    if opts['finish_date'] not in events:
-        events[opts['finish_date']] = []
-    events[opts['finish_date']].append({'type': 'supply', 'data': {'supply_id': supply, **opts}})
-
-for supply, opts in external_supplies.items():
-    if opts['finish_date'] not in events:
-        events[opts['finish_date']] = []
-    events[opts['finish_date']].append({'type': 'external_supply', 'data': {'supply_id': supply, **opts}})
-
+# Создание индекса для графика
 index = [x.strftime('%Y-%m-%d') for x in sorted(events.keys())]
 
+# Инициализация данных для графика
 graph_data = {x: [0] * (len(index) + 1) for x in categories.values()}
 
+# Подсчет начального количества товаров в каждой категории
 for category_name in graph_data:
     cat_sum = 0
     for product in products.values():
         if product['category'] == category_name:
             for shelve in product.get('shelves', []):
                 cat_sum += shelve['count']
-
     graph_data[category_name][-1] = cat_sum
 
-first = list(sorted(events.keys()))[0]
+# Добавление пустого дня перед первым событием
+first_date = sorted(events.keys())[0]
+prev_date = first_date - datetime.timedelta(days=1)
+events[prev_date] = []
+index = [prev_date.strftime('%Y-%m-%d')] + index
 
-events[first - datetime.timedelta(days=1)] = []
-index = [(first - datetime.timedelta(days=1)).strftime('%Y-%m-%d')] + index
-
+# Расчет изменений количества товаров по категориям
 diffs = {}
 offset = 0
-
 for event_date in reversed(sorted(events.keys())):
     if offset != 0:
         for category in graph_data:
@@ -201,16 +149,14 @@ for event_date in reversed(sorted(events.keys())):
         if event['type'] == 'check':
             for position in event['data']['positions']:
                 category = products[position['product_id']]['category']
-                if category not in diffs:
-                    diffs[category] = 0
-                diffs[category] += position['product_count']
+                diffs[category] = diffs.get(category, 0) + position['product_count']
         elif event['type'] == 'external_supply':
             for position in event['data']['positions']:
                 category = products[position['product_id']]['category']
-                if category not in diffs:
-                    diffs[category] = 0
-                diffs[category] -= position['product_count']
+                diffs[category] = diffs.get(category, 0) - position['product_count']
     offset += 1
 
+# Создание DataFrame и отображение графика
 data = pd.DataFrame(data=graph_data, index=index)
 st.area_chart(data)
+

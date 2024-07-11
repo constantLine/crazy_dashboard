d8d68e9ba2145c0ad85eaa2ac4d48d0378af9b6e
ConstAbz <kabzalimov@mail.ru>
2024-07-11 17:48:43 +0300

    modified:   dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,11 +3,9 @@ import streamlit as st
 import pandas as pd
 import psycopg2
 
-# Выбор схемы базы данных
 schema = st.selectbox('Select schema', ('small', 'prod'))
 
-# Установка соединения с базой данных с использованием выбранной схемы
-conn = psycopg2.connect('user=app password=verysecretpassword host=localhost dbname=appdb', options=f'-c search_path={schema}')
+conn = psycopg2.connect('user=app password=verysecretpassword host=localhost dbname=appdb', options=f'-c search_path={schema}',)
 
 def execute_query(query, params=None):
     with conn.cursor() as cur:
@@ -18,142 +16,68 @@ def get_data_dict(query):
     data = execute_query(query)
     return {row[0]: row[1] for row in data}
 
-# Запросы для получения данных
-categories = get_data_dict('SELECT category_id, name FROM categories')
-st.write("Categories loaded:", categories)
-
-
-def get_product_info():
-    query = '''
-    SELECT p.product_id, p.name, pc.category_id, c.name, pos.shelve_id, pos.count
+categories = get_data_dict('SELECT category_id, category_name FROM categories')
+products = execute_query('''
+    SELECT p.product_id, p.name, pc.category_id, c.category_name, COALESCE(pos.count, 0)
     FROM products p
     JOIN product_categories pc ON p.product_id = pc.product_id
     JOIN categories c ON pc.category_id = c.category_id
     LEFT JOIN products_on_shelves pos ON p.product_id = pos.product_id
-    GROUP BY p.product_id, p.name, pc.category_id, c.name, pos.shelve_id;
-    '''
-    data = execute_query(query)
-    products = {}
-    for row in data:
-        product_id, name, category_id, category_name, shelve_id, count = row
-        if product_id not in products:
-            products[product_id] = {
-                'name': name,
-                'category': category_name,
-                'shelves': []
-            }
-        if shelve_id is not None:
-            products[product_id]['shelves'].append({'shelve_id': shelve_id, 'count': count})
-    return products
+''')
 
-def get_checks():
-    query = '''
-    SELECT c.check_id, c.issue_date, cp.product_id, cp.product_count, cs.store_id
+checks = execute_query('''
+    SELECT c.check_id, c.issue_date, cp.product_id, cp.product_count
     FROM checks c
-    JOIN checks_stores cs ON c.check_id = cs.check_id
     JOIN product_check_positions cp ON c.check_id = cp.check_id
-    '''
-    data = execute_query(query)
-    checks = {}
-    for row in data:
-        check_id, issue_date, product_id, product_count, store_id = row
-        if check_id not in checks:
-            checks[check_id] = {
-                'issue_date': issue_date,
-                'store_id': store_id,
-                'positions': []
-            }
-        checks[check_id]['positions'].append({
-            'product_id': product_id,
-            'product_count': product_count
-        })
-    return checks
+''')
 
-def get_supplies(query):
-    data = execute_query(query)
-    supplies = {}
-    for row in data:
-        supply_id, finish_date, store_id, warehouse_id, shelve_id, product_id, product_count = row
-        if supply_id not in supplies:
-            supplies[supply_id] = {
-                'finish_date': finish_date,
-                'store_id': store_id,
-                'shelves': [],
-                'positions': []
-            }
-        if warehouse_id is not None and shelve_id is not None:
-            supplies[supply_id]['shelves'].append({
-                'warehouse_id': warehouse_id,
-                'shelve_id': shelve_id
-            })
-        if product_id is not None and product_count is not None:
-            supplies[supply_id]['positions'].append({
-                'product_id': product_id,
-                'product_count': product_count
-            })
-    return supplies
-
-products = get_product_info()
-st.write("Get product checkpoint passed")
-checks = get_checks()
-st.write("Checks checkpoint passed")
-supplies = get_supplies('''
-    SELECT s.supply_id, s.finish_date, ss.store_id, sw.warehouse_id, sw.shelve_id, sp.product_id, sp.product_count
+supplies = execute_query('''
+    SELECT s.supply_id, s.finish_date, sp.product_id, sp.product_count
     FROM supplies s
-    LEFT JOIN supplies_stores ss ON s.supply_id = ss.supply_id
-    LEFT JOIN supplies_warehouses sw ON s.supply_id = sw.supply_id
-    LEFT JOIN supplies_products sp ON s.supply_id = sp.supply_id
+    JOIN supplies_products sp ON s.supply_id = sp.supply_id
 ''')
-st.write("Supplies checkpoint passed")
-external_supplies = get_supplies('''
-    SELECT es.ext_supply_id, es.finish_date, NULL, esw.warehouse_id, esw.shelve_id, esp.product_id, esp.product_count
+
+external_supplies = execute_query('''
+    SELECT es.external_supply_id, es.finish_date, esp.product_id, esp.product_count
     FROM external_supplies es
-    LEFT JOIN external_supplies_warehouses esw ON es.ext_supply_id = esw.ext_supply_id
-    LEFT JOIN external_supplies_products esp ON es.ext_supply_id = esp.ext_supply_id
+    JOIN external_supplies_products esp ON es.external_supply_id = esp.external_supply_id
 ''')
-st.write("External Supplies checkpoint passed")
 
-
-# Создание событий для графика
 events = {}
-for check_id, opts in checks.items():
-    events.setdefault(opts['issue_date'], []).append({'type': 'check', 'data': {'check_id': check_id, **opts}})
-for supply_id, opts in supplies.items():
-    events.setdefault(opts['finish_date'], []).append({'type': 'supply', 'data': {'supply_id': supply_id, **opts}})
-for supply_id, opts in external_supplies.items():
-    events.setdefault(opts['finish_date'], []).append({'type': 'external_supply', 'data': {'supply_id': supply_id, **opts}})
 
-st.write("Events gen checkpoint passed")
+for check in checks:
+    check_id, issue_date, product_id, product_count = check
+    if issue_date not in events:
+        events[issue_date] = []
+    events[issue_date].append({'type': 'check', 'product_id': product_id, 'product_count': product_count})
 
-# Создание индекса для графика
+for supply in supplies:
+    supply_id, finish_date, product_id, product_count = supply
+    if finish_date not in events:
+        events[finish_date] = []
+    events[finish_date].append({'type': 'supply', 'product_id': product_id, 'product_count': product_count})
+
+for external_supply in external_supplies:
+    external_supply_id, finish_date, product_id, product_count = external_supply
+    if finish_date not in events:
+        events[finish_date] = []
+    events[finish_date].append({'type': 'external_supply', 'product_id': product_id, 'product_count': product_count})
+
 index = [x.strftime('%Y-%m-%d') for x in sorted(events.keys())]
-st.write("Index things:", index)
 
+graph_data = {category_name: [0] * (len(index) + 1) for category_name in categories.values()}
 
-# Инициализация данных для графика
-graph_data = {x: [0] * (len(index) + 1) for x in categories.values()}
-st.write("categories.values():", *categories.values())
-st.write("Graph data structure initialized:", graph_data)
+for product_id, name, category_id, category_name, count in products:
+    graph_data[category_name][-1] += count
 
+first = list(sorted(events.keys()))[0]
 
-# Подсчет начального количества товаров в каждой категории
-for category_name in graph_data:
-    cat_sum = 0
-    for product in products.values():
-        if product['category'] == category_name:
-            for shelve in product.get('shelves', []):
-                cat_sum += shelve['count']
-    graph_data[category_name][-1] = cat_sum
+events[first - datetime.timedelta(days=1)] = []
+index = [(first - datetime.timedelta(days=1)).strftime('%Y-%m-%d')] + index
 
-# Добавление пустого дня перед первым событием
-first_date = sorted(events.keys())[0]
-prev_date = first_date - datetime.timedelta(days=1)
-events[prev_date] = []
-index = [prev_date.strftime('%Y-%m-%d')] + index
-
-# Расчет изменений количества товаров по категориям
 diffs = {}
 offset = 0
+
 for event_date in reversed(sorted(events.keys())):
     if offset != 0:
         for category in graph_data:
@@ -161,17 +85,14 @@ for event_date in reversed(sorted(events.keys())):
     diffs.clear()
     day_events = events[event_date]
     for event in day_events:
+        category = categories[[pc[2] for pc in products if pc[0] == event['product_id']][0]]
+        if category not in diffs:
+            diffs[category] = 0
         if event['type'] == 'check':
-            for position in event['data']['positions']:
-                category = products[position['product_id']]['category']
-                diffs[category] = diffs.get(category, 0) + position['product_count']
+            diffs[category] += event['product_count']
         elif event['type'] == 'external_supply':
-            for position in event['data']['positions']:
-                category = products[position['product_id']]['category']
-                diffs[category] = diffs.get(category, 0) - position['product_count']
+            diffs[category] -= event['product_count']
     offset += 1
 
-# Создание DataFrame и отображение графика
 data = pd.DataFrame(data=graph_data, index=index)
 st.area_chart(data)
-
